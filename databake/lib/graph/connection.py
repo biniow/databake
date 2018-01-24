68071ff25b60dab731bf32ed08141cd2464ae6d4
wojtek <wojtek.biniek@gmail.com>
2018-01-24 23:23:28 +0100

    Connection class improvements and refactor

--- databake/lib/graph/connection.py
+++ databake/lib/graph/connection.py
@@ -6,14 +6,28 @@ from databake.lib.graph.pin import OUTPUT_PIN, INPUT_PIN
 
 class Connection:
     def __init__(self, from_pin, to_pin, comment=None):
-        if Connection.is_valid_pin_type_configuration(from_pin, to_pin):
-            raise PinConnectionError
+
+        Connection.validate_connection(from_pin, to_pin)
 
         self.from_pin = from_pin
         self.to_pin = to_pin
         self.comment = comment
 
     @staticmethod
-    def is_valid_pin_type_configuration(from_pin, to_pin):
-        return (from_pin.type == to_pin.type) or \
-               (from_pin.type == OUTPUT_PIN and to_pin.type == INPUT_PIN)
+    def validate_connection(from_pin, to_pin):
+        msg = None
+        if not Connection._is_valid_pin_type_configuration(from_pin, to_pin):
+            msg = f'Only OUTPUT_PIN -> INPUT_PIN connection allowed. {from_pin} -> {to_pin} is not valid'
+        elif Connection._is_circular_reference(from_pin, to_pin):
+            msg = f'Circular reference detected: {from_pin} -> {to_pin}. Check levels of associated nodes'
+
+        if msg:
+            raise PinConnectionError(msg)
+
+    @staticmethod
+    def _is_valid_pin_type_configuration(from_pin, to_pin):
+        return from_pin.type == OUTPUT_PIN and to_pin.type == INPUT_PIN
+
+    @staticmethod
+    def _is_circular_reference(from_pin, to_pin):
+        return from_pin.node.level >= to_pin.node.level
--- databake/tests/graph/test_connection.py
+++ databake/tests/graph/test_connection.py
@@ -4,32 +4,46 @@ from unittest import TestCase
 
 from databake.lib.graph.connection import Connection
 from databake.lib.graph.exceptions import PinConnectionError
+from databake.lib.graph.node import Node
 from databake.lib.graph.pin import INPUT_PIN, Pin, OUTPUT_PIN
 
 
 class TestConnection(TestCase):
+    def setUp(self):
+        self.node1 = Node(1, 'test', 'Node1')
+        self.node1.level = 1
+        self.node2 = Node(2, 'test', 'Node2')
+        self.node2.level = 2
+
+        self.pin1 = Pin('pin1', OUTPUT_PIN)
+        self.pin1.node = self.node1
+        self.pin2 = Pin('pin2', OUTPUT_PIN)
+        self.pin2.node = self.node2
+        self.pin3 = Pin('pin3', INPUT_PIN)
+        self.pin3.node = self.node1
+        self.pin4 = Pin('pin4', INPUT_PIN)
+        self.pin4.node = self.node2
+
     def test_connection_objectCreation_correctlyCreated(self):
         # Arrange
-        pin1 = Pin('pin1', INPUT_PIN)
-        pin2 = Pin('pin2', OUTPUT_PIN)
         comment = 'Dummy connection between pins'
 
         # Act
-        connection = Connection(pin1, pin2, comment)
+        connection = Connection(self.pin1, self.pin4, comment)
 
         # Assert
-        self.assertEqual(connection.from_pin, pin1)
-        self.assertEqual(connection.to_pin, pin2)
+        self.assertEqual(connection.from_pin, self.pin1)
+        self.assertEqual(connection.to_pin, self.pin4)
         self.assertEqual(connection.comment, comment)
 
     def test_connection_pinTypesWrongConfiguration_raisePinConnectionError(self):
-        # Arrange
-        pin1 = Pin('pin1', INPUT_PIN)
-        pin2 = Pin('pin2', INPUT_PIN)
-        pin3 = Pin('pin3', OUTPUT_PIN)
-        pin4 = Pin('pin4', OUTPUT_PIN)
+        # Arrange / Act / Assert
+        # Connection the same type pins should raise Exception
+        self.assertRaises(PinConnectionError, Connection, self.pin1, self.pin2)
+        self.assertRaises(PinConnectionError, Connection, self.pin3, self.pin4)
 
-        # Act / Assert
-        self.assertRaises(PinConnectionError, Connection, pin1, pin2)
-        self.assertRaises(PinConnectionError, Connection, pin3, pin4)
-        self.assertRaises(PinConnectionError, Connection, pin4, pin1)
+        # Connection from INPUT_PIN to OUTPUT_PIN is not valid
+        self.assertRaises(PinConnectionError, Connection, self.pin4, self.pin1)
+
+        # Connection from node with higher or equal level means circular reference
+        self.assertRaises(PinConnectionError, Connection, self.pin2, self.pin3)
